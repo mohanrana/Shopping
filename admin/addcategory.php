d467a096add610ba57ed10968cdcdc731467e39e
Mohan Rana <mohanrana800@gmail.com>
2018-05-25 23:55:36 +0530

    Changed Admin file

--- admin/addcategory.php
+++ admin/addcategory.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Untitled</title>
+    <title>Add Category</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
     <link rel="stylesheet" href="assets/css/user.css">
